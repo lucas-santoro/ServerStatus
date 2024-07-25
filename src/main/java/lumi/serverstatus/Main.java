802c4b9b8bf876a072a5402032277337ecf48b05
Lucas Santoro <106706839+lucas-santoro@users.noreply.github.com>
2024-07-25 09:34:02 -0300

    Creating the folder
    The plugin now creates the directory for configuration files.

--- src/main/java/lumi/serverstatus/Main.java
+++ src/main/java/lumi/serverstatus/Main.java
@@ -4,7 +4,11 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import org.slf4j.Logger;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Plugin(
         id = "serverstatus",
@@ -16,8 +20,19 @@ public class Main {
     @Inject
     private Logger logger;
 
+    @Inject
+    @DataDirectory
+    private Path dataDirectory;
+
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        logger.info("ServerStatus plugin foi inicializado!");
+        try {
+            if (!Files.exists(dataDirectory)) {
+                Files.createDirectory(dataDirectory);
+            }
+        } catch (Exception e) {
+            logger.error("It was not possible to create the plugin directory.", e);
+        }
+        logger.info("ServerStatus was enabled!");
     }
 }
